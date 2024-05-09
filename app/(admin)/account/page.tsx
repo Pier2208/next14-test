349ad781fd8dff0d45b650a621f43e089bae480e
Pier2208 <p.leroy.mtl@gmail.com>
2024-05-09 11:32:22 -0400

    add images

--- app/(admin)/account/page.tsx
+++ app/(admin)/account/page.tsx
@@ -1,11 +1,14 @@
+import Image from 'next/image';
+
 export default function AccountPage() {
   return (
-    <section>
+    <section className="max-w-lg">
       <h1 className="text-3xl my-2">ACCOUNT</h1>
-      <p className="max-w-lg">
+      <p>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente, molestias minus consequuntur nisi suscipit asperiores sit qui placeat
         consectetur illo quos in enim minima nobis facilis voluptatem a cumque harum!
       </p>
+      <Image src="/beach.jpg" width={1920} height={1276} alt="Beach" />
     </section>
   );
 }
--- app/(admin)/settings/page.tsx
+++ app/(admin)/settings/page.tsx
@@ -1,11 +1,14 @@
+import Image from 'next/image';
+
 export default function SettingsPage() {
   return (
-    <section className="">
+    <section className="max-w-lg">
       <h1 className="text-3xl my-2">SETTINGS</h1>
       <p className="max-w-lg">
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente, molestias minus consequuntur nisi suscipit asperiores sit qui placeat
         consectetur illo quos in enim minima nobis facilis voluptatem a cumque harum!
       </p>
+      <Image src="/snow.jpg" width={1920} height={1276} alt="Snow" />
     </section>
   );
 }
